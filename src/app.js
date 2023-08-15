d466f69ac6d142b8a7e9fdbbe7386f2803acf2d5
MatyBene <benedettimaty@gmail.com>
2023-08-14 23:21:24 -0300

    agrega banner y parte del formulario

--- src/app.js
+++ src/app.js
@@ -14,3 +14,7 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
