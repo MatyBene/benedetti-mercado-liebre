8b741bb9a3a9fd3445db5e4fe8e1cbfbae2b960b
MatyBene <benedettimaty@gmail.com>
2023-08-21 15:23:07 -0300

    modifica archivos app.js y package.json

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-const PORT = 3003;
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Se prendio en el puerto ${PORT}`);
 });
